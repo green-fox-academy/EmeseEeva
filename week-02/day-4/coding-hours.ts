6b9a46663d70cdca2892d0db15e0645ee93cc088
EmeseEeva <emese.hackl@gmail.com>
2018-10-29 11:33:38 +0100

    coding hours calculated

---
+++ week-02/day-4/coding-hours.ts
@@ -0,0 +1,15 @@
+var hours = 6
+var weeks = (17 * 5)
+
+var codingHours = hours * weeks
+
+console.log (codingHours);
+
+var SemeseterHours = (17 * 52)
+var percentige = (codingHours / SemeseterHours * 100)
+
+console.log (percentige + "%");
+
+
+
+
