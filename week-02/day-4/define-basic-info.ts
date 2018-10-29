93cc2907674a32ba03da88b4d168bd5e83d456f9
EmeseEeva <emese.hackl@gmail.com>
2018-10-29 12:04:40 +0100

    basic info available here

---
+++ week-02/day-4/define-basic-info.ts
@@ -0,0 +1,10 @@
+let name2 : string = "Emese";
+let age2 : number = 32;
+let height2 : number = 1.62;
+let married : boolean = false;
+
+console.log (name2);
+console.log (age2);
+console.log (height2);
+console.log (married);
+
