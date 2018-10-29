35b5e503ef3d9811ae1dccb42b6c5a472cc9c831
EmeseEeva <emese.hackl@gmail.com>
2018-10-29 17:10:11 +0100

    I draw a triangle

---
+++ week-02/day-4/[]draw-triangle.ts
@@ -0,0 +1,18 @@
+
+for (let i = 1; i<=4; i++) {
+    
+    if (i == 1){
+    console.log('*');
+    }
+    
+    else if (i == 2){
+        console.log('**');
+    }
+
+    else if (i == 3){
+        console.log('***');
+    }
+    else {
+        console.log('****');
+    }
+}
