bb334a1e62540de130843e0554f7dc019c004de0
EmeseEeva <emese.hackl@gmail.com>
2018-10-30 10:09:08 +0100

    Duplicated baseNum done

---
+++ week-02/day-5/[]doubling.ts
@@ -0,0 +1,12 @@
+// -  Create a variable named `baseNum` and assign the value `123` to it
+// -  Create a function called `doubling` that doubles it's input parameter and returns with an integer
+// -  Log the result of `doubling(baseNum)` to the console
+
+function doubling() {
+    let baseNum = 123;
+    return(baseNum * 2);
+  }
+  
+  let result = doubling();
+  console.log (result);
+
