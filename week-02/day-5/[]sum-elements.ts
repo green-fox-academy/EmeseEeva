b0f3d987814ecf32d43b2c37406181d57c84fc99
EmeseEeva <emese.hackl@gmail.com>
2018-10-30 13:41:04 +0100

    Here is the sum of the 2nd and 3rd element

---
+++ week-02/day-5/[]sum-elements.ts
@@ -0,0 +1,12 @@
+// -  Create a variable named `r` with the following content: `[54, 23, 66, 12]`
+// -  Print the sum of the second and the third element
+
+function sum(x:number, y:number) {
+    return x + y;
+
+}
+let myResult = sum(23,56);
+console.log (myResult);
+
+
+
