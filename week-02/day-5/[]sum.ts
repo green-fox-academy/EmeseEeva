00f0e2d95367e108069239084b36260be82d0581
EmeseEeva <emese.hackl@gmail.com>
2018-10-30 11:13:45 +0100

    first sum function

---
+++ week-02/day-5/[]sum.ts
@@ -0,0 +1,13 @@
+// -  Write a function called `sum` that sum all the numbers until the given parameter
+// -  The function should return the result
+
+function sum(x:number,y:number,z:number) {
+    return x + y + z;
+}
+
+let result = sum(1,2,3);
+console.log (result);
+
+
+
+
