e4fa0b8a630afa9b498909cbb368e36b2b764f64
EmeseEeva <emese.hackl@gmail.com>
2018-11-07 14:14:33 +0100

    rainbow for rainy days

---
+++ week-03/day-3/rainbow-box-function/rainbow-box-function-ts.ts
@@ -0,0 +1,25 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// Create a square drawing function that takes 2 parameters:
+// The square size, and the fill color,
+// and draws a square of that size and color to the center of the canvas.
+// Create a loop that fills the canvas with rainbow colored squares.
+
+function squareDrawing(size, color){
+    let x = (canvas.width - size) / 2;
+    let y = (canvas.height - size) / 2;
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, size, size);
+}
+   
+let color = ['blue', 'green','purple','red','orange', 'yellow'];
+
+for (let i = 6; i > 0; i--) {
+    let square = (canvas.height / 6 * i);
+    squareDrawing(square, color[6 - i]);
+  }
