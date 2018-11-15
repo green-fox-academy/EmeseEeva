5b3fd08e1e01502e0ff49f2624b2924840a39cc4
EmeseEeva <emese.hackl@gmail.com>
2018-11-15 14:58:11 +0100

    factorial 5

---
+++ week-04/day-4/refactorio.ts
@@ -0,0 +1,13 @@
+export { }
+
+// Create a recursive function called `refactorio`
+// that returns it's input's factorial
+
+function refactorio(num: number): number {
+    if (num == 0) {
+        return 1;
+    } else {
+        return num * refactorio(num - 1);
+    }
+}
+console.log(refactorio(5));
