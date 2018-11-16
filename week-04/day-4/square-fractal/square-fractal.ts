5793c576bd6ff75a679b68109bff430278da09c6
EmeseEeva <emese.hackl@gmail.com>
2018-11-16 11:06:34 +0100

    square fractals

---
+++ week-04/day-4/square-fractal/square-fractal.ts
@@ -0,0 +1,23 @@
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+
+function generateSquareRec(x: number, y: number, size: number, loopNum: number) {
+  if (loopNum > 0) {
+    let color = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;    //ctx.strokeStyle = colorPalette[Math.floor(Math.random()*colorPalette.length)];  
+    ctx.strokeStyle = color;
+    ctx.strokeRect(x, y, size, size);
+
+    size /= 3;
+    generateSquareRec(x + size, y, size, loopNum - 1);
+    generateSquareRec(x, y + size, size, loopNum - 1);
+    generateSquareRec(x + size, y + 2 * size, size, loopNum - 1);
+    generateSquareRec(x + 2 * size, y + size, size, loopNum - 1);
+
+  }
+}
+
+generateSquareRec(0, 0, canvas.width, 6);
+
