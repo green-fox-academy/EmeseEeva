056803eff191afa57c9f5dbe289d25f5925970ce
EmeseEeva <emese.hackl@gmail.com>
2018-12-06 17:36:29 +0100

    rectangle transformed

---
+++ week-07/day-4/svg-1.js
@@ -0,0 +1,6 @@
+'use strict'
+
+const rectangle = document.getElementById('very-rectangle');
+rectangle.setAttribute('x', '50');
+rectangle.setAttribute('y', '50');
+rectangle.style.fill = 'tomato';
