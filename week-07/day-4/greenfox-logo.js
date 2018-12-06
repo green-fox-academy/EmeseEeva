401f54a63f4374e6f3483184ecd9fa8751619dfb
EmeseEeva <emese.hackl@gmail.com>
2018-12-06 17:46:48 +0100

    mad fox

---
+++ week-07/day-4/greenfox-logo.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const leftEye = document.getElementById('left-eye');
+leftEye.style.fill = 'tomato';
+
+const rightEye = document.getElementById('right-eye');
+rightEye.style.fill = 'tomato';
